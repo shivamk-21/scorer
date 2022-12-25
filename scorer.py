78a95d2db1edd26af04e7b8be29c6e61d1bad70c
Shivam Kushwaha <2103shivamk@gmail.com>
2022-12-25 16:01:09 +0530

    Added Some Inline Documentation in the file

--- scorer.py
+++ scorer.py
@@ -10,15 +10,14 @@ height=app.winfo_height()
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 #Varibales
-rounds=[]
-master_team_scores=[]
+
 #Functions
-def set_round(no_teams,scores):
-    segment_buttons=[]
-    team_scores=[0]*no_teams
-    scoreTab=customtkinter.CTkEntry(rounds_frame,state='readonly',width=width*.4)
-    scoreTab.place(relx=0.4,rely=.95,anchor=tkinter.CENTER)
-    switch_var = customtkinter.StringVar(value="on")
+#Function which designs the Round's Frame
+def set_round(no_teams,scores,iniScores):
+    segment_buttons=[] #List of all segment buttons
+    switch_var = customtkinter.StringVar(value="on") #Variable for the Switch
+    team_scores=iniScores  #Initial Scores of teams
+    #Subfunction which add functionality to Switch 
     def switch_event():
         if switch_var.get()=="off":
             scoreTab.configure(state="normal")
@@ -28,8 +27,7 @@ def set_round(no_teams,scores):
             for x in range(len(s)):
                 team_scores[x]=s[x]
             scoreTab.configure(state="readonly")
-    switch = customtkinter.CTkSwitch(rounds_frame, text="Admin LOCK", command=switch_event,variable=switch_var, onvalue="on", offvalue="off")
-    switch.place(relx=0.9,rely=.95,anchor=tkinter.CENTER)
+    #Subfunction which adds functionality to the Segment Buttons
     def add_points(team_no,val):
         team_scores[team_no]+=val
         scoreTab.configure(state="normal")
@@ -38,42 +36,61 @@ def set_round(no_teams,scores):
         scoreTab.insert(0,s)
         scoreTab.configure(state="readonly")
         segment_buttons[team_no].set("unset")
+    #ScoreTab and Switch elements
+    scoreTab=customtkinter.CTkEntry(rounds_frame,state='readonly',width=width*.4)
+    scoreTab.place(relx=0.4,rely=.95,anchor=tkinter.CENTER)
+    switch = customtkinter.CTkSwitch(rounds_frame, text="Admin LOCK", command=switch_event,variable=switch_var, onvalue="on", offvalue="off")
+    switch.place(relx=0.9,rely=.95,anchor=tkinter.CENTER)
+    #Creation of segment Buttons and addition in the segment_buttons list
     for team in range(no_teams):
         segment_buttons.append(customtkinter.CTkSegmentedButton(rounds_frame,values=scores,command=partial(add_points,team)))
         segment_buttons[team].place(relx=0.5, rely=.05+(team+1)*.8/no_teams, anchor=tkinter.CENTER)
+        #Label for each Segment Button
         customtkinter.CTkLabel(rounds_frame,text="Team "+str(team+1)).place(relx=0.3, rely=.05+(team+1)*.8/no_teams, anchor=tkinter.CENTER)
-    
-def round_details(n):
+#Function to set Each Round Details
+def round_details():
+    #Removing Elements of Previous Round if they Exist
     if rounds_frame.winfo_ismapped():
         rounds_frame.forget()
         for x in rounds_frame.winfo_children():
             x.place_forget()
+    #Packing an empty Rounds Frame 
     rounds_frame.pack(side='right',fill='both',padx=20,pady=20)
+    #Functions Handling Round Creation via Dialog Boxes
     def details():
+        #Taking Relevant Details
         dialog1 = customtkinter.CTkInputDialog(text="Number of Teams:", title="Teams")
         no_teams=int(dialog1.get_input())
         dialog2 = customtkinter.CTkInputDialog(text="Score System ('/' seperated):", title="Scores")
         scores=list(map(int,dialog2.get_input().split("/")))
+        dialog3 = customtkinter.CTkInputDialog(text="Initial Team Scores (',' seperated):", title="Initial Scores")
+        ini_scores=list(map(int,dialog3.get_input().split(",")))
         detBtn.place_forget()
-        set_round(no_teams,scores)
+        #Remapping to finally create and place elements on Round's Frame
+        set_round(no_teams,scores,ini_scores)
+    #Button which opens dialogs to set Round Options
     detBtn = customtkinter.CTkButton(rounds_frame, text="Add Round Details", command=details)
     detBtn.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
+#Function to add Rounds in the SideBar 
 def add_rounds(event=None):
+    #Removing Rounds if they exist
     for x in sidebar_frame.winfo_children()[1:]:
         x.destroy()
+    #Creating Buttons for Each Round
     n=int(sidebar_textbox.get())
     sidebar_textbox.delete(0,"end")
-    rounds=[customtkinter.CTkButton(sidebar_frame,text="Round"+str(x),width=width*.05,command=partial(round_details,x)) for x in range(1,n+1)]
+    rounds=[customtkinter.CTkButton(sidebar_frame,text="Round"+str(x),width=width*.05,command=round_details) for x in range(1,n+1)]
+    #Packing all Round's Button in Sidebar
     for x in range(n):
         rounds[x].pack(pady=5)
 #Elements
 sidebar_frame = customtkinter.CTkFrame(app)
 sidebar_subframe=customtkinter.CTkFrame(sidebar_frame)
+rounds_frame=customtkinter.CTkFrame(app,width=width*.8)
 sidebar_label=customtkinter.CTkLabel(sidebar_subframe,text="Enter No of Rounds:")
 sidebar_textbox=customtkinter.CTkEntry(sidebar_subframe,width=width*.04)
 sidebar_textbox.bind("<Return>",add_rounds)
 sidebar_button=customtkinter.CTkButton(sidebar_subframe,text="Go",width=width*.02,command=add_rounds)
-rounds_frame=customtkinter.CTkFrame(app,width=width*.8)
 #Packing/Griding
 sidebar_frame.pack(side="left",fill="y",padx=10,pady=10,ipadx=5)
 sidebar_subframe.pack(pady=2,padx=2,ipadx=15,ipady=5)
